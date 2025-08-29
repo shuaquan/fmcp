3a25c7edc8542b654300e1aaf9b1c5325e67f400
shuaquan <dustinsea@hotmail.com>
2025-08-29 14:22:09 +0800

    debug

--- fmcp_server.py
+++ fmcp_server.py
@@ -18,9 +18,9 @@ mcp = FastMCP('dust_mcp_server')
 def get_current_time() -> str:
     return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-#mcp.tool 
-def get_hello_world(input: str) -> str:
-    return f"hello {input}"
+@mcp.tool 
+def get_hello_world(name: str) -> str:
+    return f"hello {name}"
 
 # 启动 mcp 服务, 监听8087端口, 本机启动, transport 使用http
 mcp.run(port=8090, host="127.0.0.1", transport="http")
